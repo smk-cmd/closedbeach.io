f494a17b0b23712ffd400baf392291c33c6ed170
onditee <TeddyOtieno.Onditi@student.csulb.edu>
2022-10-14 14:46:18 -0700

    Mint_sharks Contract
    Completed
    Test Sample also completed

--- test/sample-test.js
+++ test/sample-test.js
@@ -27,4 +27,34 @@ describe("Mint", function () {
     expect (await Beachsumos.isNftOwed(metadataURI)).to.equal(true);
 
   });
+  
+});
+
+describe("Mint_Sharks", function () {
+  it("Should Mint BeachSharks NFT when the Mint Contract is deployed", async function () {
+    const BeachSharks = await ethers.getContractFactory("BeachSharks");
+    const Beachsharks = await BeachSharks.deploy();
+    await Beachsharks.deployed();
+
+    const buyer = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+
+    const metadataURI = 'cid/test.png'
+
+    let balance = await Beachsharks.balanceOf(buyer);
+    expect(balance).to.equal(0);
+
+    const TokenMint =await Beachsharks.payToMint(buyer,metadataURI,{
+      value: ethers.utils.parseEther('0.05')
+    });
+
+    //waiting for transaction to be mined
+    await TokenMint.wait();
+
+    balance = await Beachsharks.balanceOf(buyer);
+    expect (balance).to.equal(1);
+
+    expect (await Beachsharks.isNftOwed(metadataURI)).to.equal(true);
+
+  });
+  
 });
