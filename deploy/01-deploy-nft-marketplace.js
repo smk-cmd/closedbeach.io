2c0817aabaecc2e28a58a535318f7fe1d8021d6c
smk-cmd <shane.khan@student.csulb.edu>
2022-10-14 14:37:12 -0700

    proxy added / finished deploy script

--- deploy/01-deploy-nft-marketplace.js
+++ deploy/01-deploy-nft-marketplace.js
@@ -1,2 +1,31 @@
 const { network } = require("hardhat");
 const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify")
+
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log }  = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("-----------------")
+    const box = await deploy("Box", {
+        from: deployer, 
+        args: [],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations,
+        proxy: {
+            proxyContract: "OpenZeppelinTransparentProxy",
+            viaAdminContract: {
+                name: "NFTMarketplaceProxyAdmin",
+                artifact: "NFTMarketplaceProxyAdmin",
+                        },
+        },
+    })
+
+    if (!developmentChains.Chains.includes(network.name)) && process.env.ETHERSCAN_API_){
+        log("Verifying...")
+        await verify(box.address, [])
+
+    }
+
+}
