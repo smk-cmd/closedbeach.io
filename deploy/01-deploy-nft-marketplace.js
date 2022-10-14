093eb2a82561fccc76b2fb2c4d172ad0954ad34f
ccdagreat <52589627+ccdagreat@users.noreply.github.com>
2022-10-14 15:03:59 -0700

    update scripts

--- deploy/01-deploy-nft-marketplace.js
+++ deploy/01-deploy-nft-marketplace.js
@@ -1,31 +1,33 @@
 const { network } = require("hardhat");
-const { developmentChains } = require("../helper-hardhat-config");
-const { verify } = require("../utils/verify")
-
+const {
+  developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-    const { deploy, log }  = deployments
-    const { deployer } = await getNamedAccounts()
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS;
 
-    log("-----------------")
-    const box = await deploy("Box", {
-        from: deployer, 
-        args: [],
-        log: true,
-        waitConfirmations: network.config.blockConfirmations,
-        proxy: {
-            proxyContract: "OpenZeppelinTransparentProxy",
-            viaAdminContract: {
-                name: "NFTMarketplaceProxyAdmin",
-                artifact: "NFTMarketplaceProxyAdmin",
-                        },
-        },
-    })
+  log("----------------------------------------------------");
+  const arguments = [];
+  const nftMarketplace = await deploy("NftMarketplace", {
+    from: deployer,
+    args: arguments,
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  });
 
-    if (!developmentChains.Chains.includes(network.name)) && process.env.ETHERSCAN_API_){
-        log("Verifying...")
-        await verify(box.address, [])
-
-    }
-
-}
+  // Verify the deployment
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log("Verifying...");
+    await verify(nftMarketplace.address, arguments);
+  }
+  log("----------------------------------------------------");
+};
