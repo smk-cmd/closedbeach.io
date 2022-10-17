90022f04a908d349ac1fe77e86bc6ce0c5414914
ccdagreat <52589627+ccdagreat@users.noreply.github.com>
2022-10-16 18:43:51 -0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,6 +17,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+const POLYGON_MAINNET_RPC_URL = process.env.POLYGON_MAINNET_RPC_URL
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -45,7 +50,25 @@ module.exports = {
       //   },
       saveDeployments: true,
       chainId: 5,
+      blockConfirmations: 6,
   },
+  mainnet: {
+    url: MAINNET_RPC_URL,
+    accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    //   accounts: {
+    //     mnemonic: MNEMONIC,
+    //   },
+    saveDeployments: true,
+    chainId: 1,
+    blockConfirmations: 6,
+  },
+  polygon: {
+    url: POLYGON_MAINNET_RPC_URL,
+    accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    saveDeployments: true,
+    chainId: 137,
+    blockConfirmations: 6,
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
