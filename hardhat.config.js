561ed0698deab8e40ab3513ae489a10ccd00f63c
ccdagreat <52589627+ccdagreat@users.noreply.github.com>
2022-10-17 17:00:36 -0700

    hhconfigUpdate

--- hardhat.config.js
+++ hardhat.config.js
@@ -73,8 +73,25 @@ module.exports = {
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+},    contractSizer: {
+  runOnCompile: false,
+  only: ["NftMarketplace"],
+},
+namedAccounts: {
+  deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+  },
+  player: {
+      default: 1,
+  },
+},
 };
