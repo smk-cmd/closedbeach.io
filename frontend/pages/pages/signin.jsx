9051a36eddfb399b881e2068e7f5e957fcd41944
smk-cmd <shane.khan@student.csulb.edu>
2022-11-09 19:15:10 -0800

    revised sign in page

--- frontend/pages/pages/signin.jsx
+++ frontend/pages/pages/signin.jsx
@@ -1,7 +1,22 @@
+import { useConnect } from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import axios from 'axios';
+
 function SignIn() {
+    const { connectAsync } = useConnect();
+
+    const handleAuth = async () => {
+        const { account, chain } = await connectAsync({ connector: new InjectedConnector() });
+
+        const userData = { address: account, chain: chain.id, network: 'evm' };
+
+        console.log(userData)
+    };
+
     return (
         <div>
             <h3>Web3 Authentication</h3>
+            <button onClick={() => handleAuth()}>Authenticate via Metamask</button>
         </div>
     );
 }
