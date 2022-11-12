ab381f153ba347cab246a4c7b7bfd9fdaed79a52
smk-cmd <shane.khan@student.csulb.edu>
2022-11-12 13:38:10 -0800

    trying to add session provider

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,14 @@
+import { unstable_getServerSession } from "next-auth/next"
+import { authOptions } from './api/auth/[...nextauth]'
+
+export async function getServerSideProps({ req, res }) {
+  return {
+    props: {
+      session: await unstable_getServerSession(req, res, authOptions)
+    }
+  }
+}
+
 import "./app.css";
 import Layout from "./components/Layout/Layout";
 
