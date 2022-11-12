865071556f515b1a4f82ac6daa98489c48c64907
smk-cmd <shane.khan@student.csulb.edu>
2022-11-12 12:41:53 -0800

    extending handleauth for request message endpoint

--- frontend/pages/pages/signin.jsx
+++ frontend/pages/pages/signin.jsx
@@ -1,16 +1,33 @@
-import { useConnect } from 'wagmi';
+import { useAccount, useConnect, useSignMessage, useDisconnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import axios from 'axios';
 
 function SignIn() {
     const { connectAsync } = useConnect();
+    const { disconnectAsync } = useDisconnect();
+    const { isConnected } = useAccount();
+    const { signMessageAsync } = useSignMessage();
 
     const handleAuth = async () => {
+        //disconnects the web3 provider if it's already active
+        if (isConnected) {
+            await disconnectAsync();
+        }
+        // enabling the web3 provider metamask
         const { account, chain } = await connectAsync({ connector: new InjectedConnector() });
 
         const userData = { address: account, chain: chain.id, network: 'evm' };
+        // making a post request to our 'request-message' endpoint
+        const { data } = await axios.post('/api/auth/request-message', userData, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const message = data.message;
+        // signing the received message via metamask
+        const signature = await signMessageAsync({ message });
 
-        console.log(userData)
+        console.log(signature)
     };
 
     return (
