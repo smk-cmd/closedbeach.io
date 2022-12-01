97427295a9eb8a5339f4a1779926ee8916b97e04
ccdagreat <52589627+ccdagreat@users.noreply.github.com>
2022-11-30 20:56:07 -0800

    finished basic styling

--- frontendnext/pages/index.js
+++ frontendnext/pages/index.js
@@ -1,16 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Closed Beach</title>
-        <meta name="description" content="Closed Beach" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Hi!
-    </div>
-  )
+    return <div className={styles.container}>Hi!</div>
 }
