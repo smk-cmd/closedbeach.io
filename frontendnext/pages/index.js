05ca67f4528fb3fe406ae2a12419c4cd6cc5691b
TylerM1105 <tyler.mcdonald1105@gmail.com>
2022-11-30 22:14:16 -0800

    Start to implement backend database elements

--- frontendnext/pages/index.js
+++ frontendnext/pages/index.js
@@ -1,6 +1,16 @@
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    return <div className={styles.container}>Hi!</div>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Closed Beach</title>
+        <meta name="description" content="Closed Beach NFT marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Index
+    </div>
+  )
 }
