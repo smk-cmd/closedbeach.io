2716647533e74332ca75143a40dc11c6f9543944
onditee <TeddyOtieno.Onditi@student.csulb.edu>
2022-12-07 16:56:58 -0800

    Import Statements
    These are the import statements we will use for this  and default function

---
+++ frontendnext/components/UpdateListingModal.js
@@ -0,0 +1,27 @@
+import { Modal, Input, useNotification } from "web3uikit"
+import { useState } from "react"
+import { useWeb3Contract } from "react-moralis"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import { ethers } from "ethers"
+
+export default function UpdateListingModal({
+    nftAddress,
+    tokenId,
+    isVisible,
+    marketplaceAddress,
+    onClose,
+}) {
+    const dispatch = useNotification()
+
+    const [priceToUpdateListingWith, setPriceToUpdateListingWith] = useState(0)
+
+    const handleUpdateListingSuccess = () => {
+        dispatch({
+            type: "success",
+            message: "listing updated",
+            title: "Listing updated - please refresh (and move blocks)",
+            position: "topR",
+        })
+        onClose && onClose()
+        setPriceToUpdateListingWith("0")
+    }
